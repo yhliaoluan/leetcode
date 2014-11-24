db8c3c45407ce298f3d235884d3f59e9455377dc
yhliaoluan <tianyuan86@163.com>
2014-11-24 14:36:45 +0800

    solve subsets

--- src/subsets.cpp
+++ src/subsets.cpp
@@ -28,31 +28,29 @@ public:
     vector<vector<int> > subsets(vector<int> &s) {
         sort(s.begin(), s.end());
         vector<vector<int> >v;
+        vector<int> zero_array;
+        v.push_back(zero_array);
 
-        for (int i = 0; i <= s.size(); i++) {
-            vector<int> index;
-            int index = 0;
-            for (int j = 0; j < i; j++) {
-                index.push_back(index++);
-            }
-            for (;;) {
-                vector<int> tmp;
-                for (int j = 0; j < i; j++) {
-                    tmp.push_back(s[j]);
-                }
-                v.push_back(tmp);
-
-                if (index.size() == 0) {
-                    break;
-                }
-            }
+        for (int i = 0; i < s.size(); i++) {
+            vector<int> array;
+            push(s, array, v, 0, i);
         }
 
         return v;
     }
 
 private: 
-    void push(vector<int> &s, vector<int> &to, int index) {
+    void push(vector<int> &s, vector<int> &added,
+        vector<vector<int> > &added_to, int index, int remain) {
+        for (int i = index; i < s.size() - remain; i++) {
+            vector<int> copied = added;
+            copied.push_back(s[i]);
+            if (remain == 0) {
+                added_to.push_back(copied);
+            } else {
+                push(s, copied, added_to, i + 1, remain - 1);
+            }
+        }
     }
 };
 
@@ -63,8 +61,14 @@ int main(int argc, char **argv) {
         array.push_back(value);
     }
 
+    printf("input ");
+    for (int i = 0; i < array.size(); i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
     Solution s;
     vector<vector<int> > v = s.subsets(array);
+    printf("get size %d\n", v.size());
     for (int i = 0; i < v.size(); i++) {
         for (int j = 0; j < v[i].size(); j++) {
             printf("%d ", v[i][j]);
