d2a50a3fa9f209b32d5cbb348ce4e9678c410a15
ytian <tianyuan86@163.com>
2015-03-26 01:48:35 +0800

    commit

---
+++ src/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,27 @@
+/*
+ Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+ Do not allocate extra space for another array, you must do this in place with constant memory.
+
+ For example,
+ Given input array A = [1,1,2],
+
+ Your function should return length = 2, and A is now [1,2]. 
+ */
+
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        if (n < 2) {
+            return n;
+        }
+        int tail = n;
+        int index = 1;
+        while (index < tail) {
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+    return 0;
+}
