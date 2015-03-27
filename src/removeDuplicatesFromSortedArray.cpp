f5e0f0a121874acd509874cb44f1dc5301c01d56
yhliaoluan <tianyuan86@163.com>
2015-03-27 10:35:33 +0800

    remove duplicates from sorted array

--- src/removeDuplicatesFromSortedArray.cpp
+++ src/removeDuplicatesFromSortedArray.cpp
@@ -8,6 +8,8 @@
 
  Your function should return length = 2, and A is now [1,2]. 
  */
+#include <iostream>
+using namespace std;
 
 class Solution {
 public:
@@ -15,13 +17,32 @@ public:
         if (n < 2) {
             return n;
         }
-        int tail = n;
-        int index = 1;
-        while (index < tail) {
+        int left = 1;
+        int right = 1;
+        int last = A[0];
+        while (right < n) {
+            if (A[right] == last) {
+                right++;
+            } else {
+                last = A[left] = A[right];
+                left++;
+                right++;
+            }
         }
+        return left;
     }
 };
 
 int main(int argc, char **argv) {
+    int *a = new int[argc - 1];
+    for (int i = 1; i < argc; i++) {
+        a[i - 1] = atoi(argv[i]);
+    }
+    Solution s;
+    int n = s.removeDuplicates(a, argc - 1);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
