ce77d9d1c06ede70f6c5743631457bb1bac11bb0
yhliaoluan <tianyuan86@163.com>
2015-04-02 11:17:20 +0800

    longest substring without repeating characters, a better solution

--- src/longestSubstringWithoutRepeatingCharacters.cpp
+++ src/longestSubstringWithoutRepeatingCharacters.cpp
@@ -1,52 +1,35 @@
 /*
    Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
    */
+#include <iostream>
 #include <map>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
-
-typedef struct Node {
-    int index;
-    char val;
-    Node(int x, char y) : index(x), val(y) {}
-} Node;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        int i = 0;
+        int j = 0;
+        unordered_map<char, int> m;
         int longest = 0;
-        map<char, Node> m;
-        for (int j = 0; j < s.size(); j++) {
-            map<char, Node>::iterator it = m.find(s[j]);
-            if (it != m.end()) {
-                if (m.size() > longest) {
-                    longest = m.size();
+        while (j < s.size()) {
+            if (m.find(s[j]) != m.end()) {
+                longest = max(j - i, longest);
+                while (i < j && s[i] != s[j]) {
+                    m.erase(s[i++]);
                 }
-                removeLessThan(m, it->second.index + 1);
-            }
-            printf("insert %c %d\n", s[j], j);
-            m.insert(map<char, Node>::value_type(s[j], Node(j, s[j])));
-        }
-
-        if (m.size() > longest) {
-            longest = m.size();
-        }
-        return longest;
-    }
-
-private:
-    void removeLessThan(map<char, Node> &m, int index) {
-        map<char, Node>::iterator it = m.begin();
-        for (; it != m.end();) {
-            if (it->second.index < index) {
-                printf("erase %c %d\n", it->second.val, it->second.index);
-                it = m.erase(it);
+                m.erase(s[i++]);
             } else {
-                it++;
+                pair<char, int> p(s[j], j);
+                m.insert(p);
+                j++;
             }
         }
+        return max(j - i, longest);
     }
 };
 
