6d3a3eb0bbbd1796332b66ddb7bd055ed25596d8
yhliaoluan <tianyuan86@163.com>
2015-04-10 16:40:33 +0800

    spiral matrix

--- src/spiralMatrix.cpp
+++ src/spiralMatrix.cpp
@@ -17,22 +17,22 @@ public:
     vector<int> spiralOrder(vector<vector<int>> &matrix) {
         vector<int> result;
         if (matrix.empty()) return result;
-        result = matrix[0];  // no need to check the first row
-        int dirs[4][2] = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};  // direction offsets
-        int d = 0;  // direction index
+        int dirs[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int pos[2] = {0, -1};
         int m = matrix.size();
         int n = matrix[0].size();
-        int pos[2] = {0, n - 1};  // start from the top right corner
-        int i = (m - 1) * n;  // number of the rest numbers
+        int i = m * n;
+        int d = 0;
         while (i > 0) {
-            for (int j = 1; j < m; j++) {
+            for (int j = 0; j < n; j++) {
                 i--;
-                pos[0] += dirs[d][0]; pos[1] += dirs[d][1];
+                pos[0] += dirs[d][0];
+                pos[1] += dirs[d][1];
                 result.push_back(matrix[pos[0]][pos[1]]);
             }
-            m--;  // decrease the size of row or column
-            swap(m, n);  // switch between horizontal and vertical mode
-            d = (d + 1) % 4;  // loop between direction offsets
+            m--;
+            swap(m, n);
+            d = (d + 1) % 4;
         }
         return result;
     }
