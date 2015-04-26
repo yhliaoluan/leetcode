2874c6d57ab5bb524c950d0803f44bcfb5384889
ytian <tianyuan86@163.com>
2015-04-26 23:46:52 +0800

    partial commit

---
+++ src/sqrt.cpp
@@ -0,0 +1,30 @@
+/*
+ *Implement int sqrt(int x).
+
+Compute and return the square root of x.
+ * */
+#include "header.h"
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return bin_src(x, 0, min(x, 46340));
+    }
+    int bin_src(int x, int low, int up) {
+        if (low == up) { return low; }
+        int bin = (low + up) / 2;
+        if (bin * bin == x) {
+            return bin; 
+        } else if (bin * bin > x) {
+            return bin_src(x, low, bin);
+        } else {
+            return bin_src(x, bin + 1, up);
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+    Solution s;
+    cout << s.mySqrt(atoi(argv[1])) << endl;
+    return 0;
+}
