e74fe17e1c9d68dc33735d4e6655b65ad959d5f8
ytian <tianyuan86@163.com>
2015-06-10 23:08:43 +0800

    unfinished

---
+++ src/reverseLinkedList2.cpp
@@ -0,0 +1,43 @@
+/*
+ Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+ * */
+#include "header.h"
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode *left = &dummy;
+        for (; m > 1; m--, n--) {
+            left = left->next;
+        }
+        ListNode *right = left;
+    }
+};
+
+int main(int argc, char **argv) {
+    return 0;
+}
