6e5005964d94f84511fbd79baba0841cce6f83d1
yhliaoluan <tianyuan86@163.com>
2015-06-12 15:10:23 +0800

    unfinished

---
+++ src/uniqueBinarySearchTrees2.cpp
@@ -0,0 +1,33 @@
+/*
+Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+*/
+#include "header.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+    }
+};
+
+int main(int argc, char **argv) {
+    return 0;
+}
