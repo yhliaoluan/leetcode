93c9ad8fa36ed774ab750c46ff7d90cf3b24da39
ytian <tianyuan86@163.com>
2015-06-28 16:49:13 +0800

    distinct subsequences

--- src/distinctSubsequences.cpp
+++ src/distinctSubsequences.cpp
@@ -16,12 +16,23 @@ public:
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        if (m == 0 || n == 0) { return 0; }
-        vector<vector<int> > f;
-        for (int i = 0; i < m; i++) {
-            f.push_back(vector<int>(n, 0));
+        vector<vector<int> > dp;
+        for (int i = 0; i < m + 1; i++) {
+            dp.push_back(vector<int>(n + 1, 0));
         }
-
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] != t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
 
