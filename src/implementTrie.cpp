b81804d5e48d92de016b18a805a0650919a4529c
ytian <tianyuan86@163.com>
2015-08-13 23:35:10 +0800

    implement trie

--- src/implementTrie.cpp
+++ src/implementTrie.cpp
@@ -8,11 +8,10 @@ You may assume that all inputs are consist of lowercase letters a-z.
 
 class TrieNode {
 public:
-    TrieNode(string val) : value(val), left(NULL), right(NULL) { }
-    TrieNode() : value(""), left(NULL), right(NULL) { }
-    TrieNode *left;
-    TrieNode *right;
-    string value;
+    TrieNode() : isLeaf(false), nodes(26, NULL) { }
+    ~TrieNode() { for (auto n : nodes) if (n) delete n; }
+    vector<TrieNode *> nodes;
+    bool isLeaf;
 };
 
 class Trie {
@@ -20,64 +19,44 @@ public:
     Trie() {
         root = new TrieNode();
     }
+    ~Trie() {
+        delete root;
+    }
 
     // Inserts a word into the trie.
     void insert(string word) {
         TrieNode *cur = root;
-        while (cur) {
-            int res = cur->value.compare(word);
-            if (res < 0) {
-                if (cur->left == NULL) {
-                    cur->left = new TrieNode(word);
-                    return;
-                } else {
-                    cur = cur->left;
-                }
-            } else if (res > 0) {
-                if (cur->right == NULL) {
-                    cur->right = new TrieNode(word);
-                    return;
-                } else {
-                    cur = cur->right;
-                }
-            } else {
-                return;
+        for (int i = 0; i < word.size(); i++) {
+            int index = word[i] - 'a';
+            if (cur->nodes[index] == NULL) {
+                cur->nodes[index] = new TrieNode();
             }
+            cur = cur->nodes[index];
         }
+        cur->isLeaf = true;
     }
 
     // Returns if the word is in the trie.
     bool search(string word) {
         TrieNode *cur = root;
-        while (cur) {
-            int res = cur->value.compare(word);
-            if (res < 0) {
-                cur = cur->left;
-            } else if (res > 0) {
-                cur = cur->right;
-            } else {
-                return true;
-            }
+        for (int i = 0; i < word.size(); i++) {
+            int index = word[i] - 'a';
+            if (cur->nodes[index] == NULL) { return false; }
+            cur = cur->nodes[index];
         }
-        return false;
+        return cur->isLeaf;
     }
 
     // Returns if there is any word in the trie
     // that starts with the given prefix.
     bool startsWith(string prefix) {
         TrieNode *cur = root;
-        while (cur) {
-            int res = cur->value.substr(0,
-                min(prefix.size(), cur->value.size())).compare(prefix);
-            if (res < 0) {
-                cur = cur->left;
-            } else if (res > 0) {
-                cur = cur->right;
-            } else {
-                return true;
-            }
+        for (int i = 0; i < prefix.size(); i++) {
+            int index = prefix[i] - 'a';
+            if (cur->nodes[index] == NULL) { return false; }
+            cur = cur->nodes[index];
         }
-        return false;
+        return true;
     }
 
 private:
@@ -91,6 +70,24 @@ private:
 
 int main(int argc, char **argv) {
     Trie trie;
-    trie.search("a");
+    int opt = 1;
+    for (int i = 1; i < argc; i++) {
+        if (string(argv[i]).compare("1") == 0) {
+            opt = 1;
+        } else if (string(argv[i]).compare("2") == 0) {
+            opt = 2;
+        } else if (string(argv[i]).compare("3") == 0) {
+            opt = 3;
+        } else {
+            if (opt == 1) {
+                trie.insert(argv[i]);
+                cout << "insert " << argv[i] << endl;
+            } else if (opt == 2) {
+                cout << "search " << argv[i] << " " << trie.search(argv[i]) << endl;
+            } else if (opt == 3) {
+                cout << "start with " << argv[i] << " " << trie.startsWith(argv[i]) << endl;
+            }
+        }
+    }
     return 0;
 }
